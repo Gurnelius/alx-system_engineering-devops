d0ada23f8f21f63bdc9a804dc65a946258894fcf
Gurnelius <terergurnelius@gmail.com>
2024-03-22 16:24:49 +0300

    Initial commit

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,4 @@
 # kill the process killmenow
-exec { 'pkill':
-	command =>  'pkill -f killmenow',
-	path 	=>  ['/usr/bin', '/usr/sbin', '/bin'],
-	onlyif  =>  'pgrep -f killmenow',
+exec{'pkill -f killmenow':
+path  => '/usr/bin/:/usr/local/bin/:/bin/'
 }
