5060fa00af0520028030e086b7302297f36d1ed1
Gurnelius <terergurnelius@gmail.com>
2024-03-22 16:31:15 +0300

    Initial commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,25 @@
-# installed flask version is 2.1.0
-package {'flask':
-ensure    => '2.1.0',
-provider  => 'pip3',
+# Install flask and Werkzeug
+
+# ensure that python is installed
+package { 'python3.8':
+  ensure => present,
+}
+
+# ensure pip is installed
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 2.1.0 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
