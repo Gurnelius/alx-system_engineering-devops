9cae6ec57007660fab71016378bb48d5768ec077
Gurnelius Terer <terergurnelius@gmail.com>
2024-04-22 16:45:33 +0300

    Initial commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    Consume an API https://jsonplaceholder.typicode.com/todos/1
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    url = url + "/" + userId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todo_url = url + "/todos"
+    response = requests.get(todo_url)
+    tasks = response.json()
+
+    with open('{}.csv'.format(userId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                .format(userId, username, task.get('completed'),task.get('title')))
