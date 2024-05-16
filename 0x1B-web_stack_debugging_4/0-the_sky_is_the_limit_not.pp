45ccee99a437249d424b6506dea075a6b48d66fc
Gurnelius Terer <terergurnelius@gmail.com>
2024-05-16 21:32:38 +0300

    Initial commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# This manuscript increases the amount of traffic an Nginx server can handle
+# increases the amount of traffic an Nginx server can handle
 
-# Increase the ULIMIT of the default file
+# increase the ULIMIT in the default file
 file { 'fix-for-nginx':
   ensure  => 'file',
   path    => '/etc/default/nginx',
   content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
 }
 
-# Restart Nginx
+# Restart Nginx server
 -> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/',
